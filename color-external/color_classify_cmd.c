6c14d4fc23aaacc1e395e7779b8edfecda4d1c86
Fefo <fefo@grad.icmc.usp.br>
2011-11-01 15:32:16 -0200

    finished color_classify_cmd

--- color-external/color_classify_cmd.c
+++ color-external/color_classify_cmd.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 float MAX (float a, float b, float c);
 float MIN (float a, float b, float c);
@@ -14,44 +15,193 @@ int main (int argc, const char * argv[])
     float hue;
     float sat;
     float val;
+    char label[20] = "none";
+    char secondary_label[20] = "none";
+    char certainty_level[20] = "certain";
     
     RGBtoHSV(red, green, blue, &hue, &sat, &val);
     
-    if (val < 0.2 && sat < 0.8) {
-        printf("Black\n");
-        return 0;
-    } else if (val > 0.8 && sat < 0.1) {
-        printf("White\n");
-        return 0;
-    }
-    
-    // At this point we have
-    // val > 0.2 | (val < 0.8 | sat > 0.1)
-    
-    if (hue < 30 || hue > 330) {
-        printf("Red\n");
-    } else if (hue > 90 && hue < 150) {
-        printf("Green\n");
-    } else if (hue > 185 && hue < 270) {
-        printf("Blue\n");
-    } else { // disp 'unreliable color...';
-        printf("Warning: Unreliable!    -----> ");
-        // in the real system you just discard this estimate and use the previous
-        // estimate (e.g. previous frame) at this point
-        if (hue >= 30 && hue <= 90) {
-            printf("Guessing yellow\n");
-        } else if (hue >= 150 && hue <= 185) {
-            if (hue >= 180) {
-                printf("Guessing blue\n");
-            } else {
-                printf("Guessing blue-green\n");
+    if (val < 0.3) {
+        if (sat > 0.3 && val > 0.2) {
+            strcpy(secondary_label, "black");
+            if (sat > 0.6 && (hue >= 65 && hue <= 170)) {
+                strcpy(label, "green");
+                printf("%s %s %s\n", label, secondary_label, certainty_level);
+                return 0;
+            } 
+            else if (sat > 0.8) {
+                strcpy(certainty_level, "unreliable");
+            } 
+            else {
+                strcpy(certainty_level, "good guess");
             }
-        } else if (hue >= 270 && hue <= 330) {
-            printf("Guessing purple-pink\n");
+            // will proceed below to guessing colors
+        }
+        else {
+            strcpy(label, "black");
+        }
+    }
+    else if ((val > 0.8 && sat < 0.2) || (val > 0.7 && sat < 0.1) || (val > 0.6 && sat < 0.05)) {
+        strcpy (label, "white");
+        if (sat > 0.1) {
+            // light baby colors; could be white with offset colorbalance.
+            strcpy(certainty_level, "unreliable"); 
+            // will proceed below to guessing colors
+            strcpy(label, "none");
+            strcpy(secondary_label, "white");
+        }
+        else {
+            if (val < 0.7) {
+            strcpy(secondary_label, "gray");
+            strcpy(certainty_level, "good guess");
+            }
+            printf("%s %s %s\n", label, secondary_label, certainty_level);
+            return 0;
+        }
+    }
+    else if ((val < 0.65 && sat < 0.15) || (val < 0.7 && sat < 0.1) || (val < 0.5 && sat < 0.3 && (hue < 40 && hue > 10))) { // heuristica ~ marrons
+        if (val < 0.50) {
+            strcpy(label, "black");
+            strcpy(certainty_level, "good guess");
+            if (val > 0.40) {
+                strcpy(secondary_label, "gray");
+            }
+            printf("%s %s %s\n", label, secondary_label, certainty_level);
+            return 0;
+        }
+        else {
+            if (sat < 0.08) {
+                strcpy(certainty_level, "good guess");
+                strcpy(label, "white");
+                strcpy(secondary_label, "gray");
+                printf("%s %s %s\n", label, secondary_label, certainty_level);
+                return 0;
+            }
+            else {
+                strcpy(certainty_level, "unreliable");
+                strcpy(secondary_label, "gray");
+            }
         }
     }
     
-    
+    //RED
+    if (hue < 30 || hue > 330) {
+        if (hue > 10 && hue <= 30 && sat < 0.4) {
+            if (hue > 20) { 
+                strcpy(label, "yellow");
+                strcpy(certainty_level, "good guess");
+                if (val < 0.7) {
+                    strcpy(secondary_label, "gray");
+                }
+            }
+            else { 
+                strcpy(label, "red");
+                strcpy(certainty_level, "unreliable");
+                if (val < 0.7) {
+                    strcpy(secondary_label, "gray");
+                }
+            }
+        }
+        else {
+            strcpy(label, "red");
+            if ((sat < 0.3 && val < 0.6) | (sat < 0.4 & val < 0.35)) {
+                if (strcmp(certainty_level, "certain") == 0) {
+                    strcpy(certainty_level,  "good guess");
+                }
+            }
+        }
+    }
+    // GREEN
+    else if (hue > 80 && hue < 170) {
+        strcpy(label, "green");
+        if (sat < 0.2) {
+            if (strcmp(certainty_level, "certain") == 0) {
+                strcpy(certainty_level, "good guess");
+            }
+        }
+        if (hue > 160 && (sat < 0.6 || val > 0.6)) {
+            strcpy(certainty_level, "unreliable");
+            strcpy(secondary_label, "blue");
+        }
+    }
+    // BLUE
+    else if (hue > 185 && hue < 270) {
+        if (sat < 0.3) { 
+            if (strcmp(certainty_level, "certain") == 0) {
+                strcpy(certainty_level, "good guess");
+            }
+            if (val > 0.75) {
+                strcpy(label, "white");
+                strcpy(secondary_label, "blue");
+                if (strcmp(certainty_level, "certain") == 0) {
+                    strcpy(certainty_level, "good guess");
+                }
+            }
+            else {
+                strcpy(label, "blue");
+                if (val < 0.6) {
+                    strcpy(secondary_label, "gray");
+                }
+                else {
+                    strcpy(secondary_label, "white");
+                }
+            }
+        }
+        else {
+            strcpy(label, "blue");
+        }
+    }
+    // remaining YELLOW + OTHER cases
+    else {
+        if (sat < 0.5) {
+            strcpy(certainty_level, "unreliable");
+        }
+        // in the real system you just discard this estimate and use the previous
+        // estimate (e.g. previous frame) at this point
+        if (hue >= 30 && hue <= 80) {
+            if (hue >= 70 && val <= 70) {
+                strcpy(label, "green");
+            }
+            else {
+                strcpy(label, "yellow");
+            }
+        }
+        else {
+            if (strcmp(certainty_level, "certain") == 0) {
+                    strcpy(certainty_level, "good guess");
+            }
+            if (hue >= 150 && hue <= 185) {
+                // hard test near cyan and put a secondary label.
+                if (hue >= 180) {
+                    strcpy(label, "blue");
+                }
+                else if (hue < 170) {
+                    strcpy(label, "green");
+                    strcpy(secondary_label, "blue");
+                }
+                else {
+                    strcpy(label, "blue");
+                    strcpy(secondary_label, "green");
+                }
+            }
+            else if (hue >= 270 && hue <= 330) {
+                // hard test near magenta and put a secondary label.
+                if (hue < 280) {
+                    strcpy(label, "blue");
+                    if (strcmp(secondary_label, "none") == 0) {
+                        strcpy(secondary_label, "purple-pink-lavender");
+                    }
+                }
+                else {
+                    strcpy(label, "red");
+                    if (strcmp(secondary_label, "none") == 0) {
+                        strcpy(secondary_label, "purple-pink-magenta");
+                    }
+                }
+            }
+        }
+    }
+    printf("%s %s %s\n", label, secondary_label, certainty_level);
     return 0;
 }
 
